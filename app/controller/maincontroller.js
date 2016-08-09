8662e286a8f027eb7fd8dc07c3eb7beb26e8e0bd
Ander OW PC <khanhduy.39n@gmail.com>
2016-08-09 16:10:40 +0700

    88

---
+++ app/controller/maincontroller.js
@@ -0,0 +1,18 @@
+/**
+ * Main application controller
+ *
+ * You can use this controller for your whole app if it is small
+ * or you can have separate controllers for each logical section
+ *
+ */
+;(function() {
+
+  app.controller('MainController', MainController);
+
+  MainController.$inject = ['LocalStorage', 'QueryService'];
+
+
+  function MainController(LocalStorage, QueryService) {
+    var self = this;
+  }
+})();
---
+++ components/directives/owl.slider.directive.js
@@ -0,0 +1,29 @@
+;(function() {
+
+
+  'use strict';
+
+
+  /**
+   * Owl slider directive
+   *
+   * Usage:
+   * <div myslider>
+   *   <ul class="slider">
+   *     ...
+   *   </ul>
+   * </div>
+   *
+   * or
+   *
+   * <myslider>
+   *   <ul class="slider">
+   *     ...
+   *   </ul>
+   * </myslider>
+   *
+   * @url http://owlgraphic.com/owlcarousel/index.html#customizing
+   *
+   */
+
+})();
